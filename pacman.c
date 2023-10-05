7ab3256c16a8185d5ea23be0d5f084e00224d115
Daniel Soares <campossoares.daniel@gmail.com>
2023-10-05 12:00:37 -0300

    character-movement

--- pacman.c
+++ pacman.c
@@ -39,13 +39,62 @@ void lermapa(){
     }
 }
 
-int main (){
+void move(char direcao){
+    int x;
+    int y;
 
-    lermapa();
+    // Acha o personagem
+    for(int i = 0; i < linhas; i++){
+        for (int j = 0; j < colunas; j++){
+            if(mapa[i][j] == '@'){
+                x = i;
+                y = j;
+                break;
+            }
+        }
+    }
+    
+    switch (direcao)
+    {
+    case 'a':
+        mapa[x][y-1] = '@';
+        break;
+    case 'w':
+        mapa[x-1][y] = '@';
+        break;
+    case 's':
+        mapa[x+1][y] = '@';
+        break;
+    case 'd':
+        mapa[x][y+1] = '@';
+        break;
+    }
+    mapa[x][y] = '.';
 
+}
+
+int acabou() {
+    return 0;
+}
+
+void imprimemapa(){
     for (int i = 0; i < 5; i++){
         printf("%s\n", mapa[i]);
     }
+}
+
+int main (){
+
+    lermapa();
+    do
+    {
+        imprimemapa();
+        char comando;
+        scanf(" %c", &comando);
+        move(comando);
+    } while (!acabou());
+    
+    
 
     liberamapa();
 }
--- pacman.h
+++ pacman.h
@@ -1,3 +1,6 @@
 void liberamapa();
 void lermapa();
 void alocamapa();
+void move();
+int acabou();
+void imprimemapa();
